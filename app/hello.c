630b59fa8cbf4314f58c95ccad1e4c11ee1a75b0
rahul mahadev <rahul.mahadev7@gmail.com>
2016-02-03 19:08:09 +0530

    Started dev on quiz

--- app/hello.c
+++ app/hello.c
@@ -9,57 +9,55 @@
 #include <ta_hello_world.h>
 #include <err.h>
 int main(void){
- 	printf("Hello Universe\n");
  	TEEC_Result res;
 	TEEC_Context ctx;
 	TEEC_Session sess;
 	TEEC_Operation op;
 	TEEC_UUID uuid = TA_HELLO_WORLD_UUID;
 	uint32_t err_origin;
-	res = TEEC_InitializeContext(NULL, &ctx);
+	res = TEEC_InitializeContext(NULL, &ctx); // This basically checks if a TEE is present in the system
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);   
 	/*
-	 * Open a session to the "hello world" TA, the TA will print "hello
-	 * world!" in the log when the session is created.
+	 * Open a session to the  TA
 	 */
 	res = TEEC_OpenSession(&ctx, &sess, &uuid,TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",res, err_origin);
-	/*
-	 * Execute a function in the TA by invoking it, in this case
-	 * we're incrementing a number.
-	 *
-	 * The value of command ID part and how the parameters are
-     * interpreted is part of the interface provided by the TA.
-	 */
+
 
 	/*
 	 * Prepare the argument. Pass a value in the first parameter,
 	 * the remaining three parameters are unused.
 	 */
+
 	memset(&op, 0, sizeof(op));
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
-	printf("Enter the number you want to find the factorial of \n");
-	scanf("%d",&op.params[0].value.a );
-
-	printf("Invoking TA to find factorial of %d\n", op.params[0].value.a);
-	res = TEEC_InvokeCommand(&sess, TA_HELLO_WORLD_CMD_INC_VALUE, &op,
+	
+	
+	printf("Moving to secure world to start quiz \n");
+	//
+	op.params[0].value.a = 4;
+	res = TEEC_InvokeCommand(&sess, TA_HELLO_WORLD_CMD_START_QUIZ, &op,
 				 &err_origin);
 	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
-			res, err_origin);
-	printf("TA found factorial as %d\n", op.params[0].value.a);
-
+		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res, err_origin);
+		
+	printf("TEE returned value  %d\n", op.params[0].value.a);
 	/*
-	 * We're done with the TA, close the session and
-	 * destroy the context.
+		if(op.params[0].value.a == 1000)
+		{
+			printf("1000 returned \n");
+			break;
+		}
+	*/
+	
+	printf("Quiz eneded\n");
+	/* 
+	 * Close the session 
 	 *
-	 * The TA will print "Goodbye!" in the log when the
-	 * session is closed.
 	 */
-
 	TEEC_CloseSession(&sess);
 
 	TEEC_FinalizeContext(&ctx);
