00717bb67f4a1599dfb24823c25adf5c28a2a85c
rahul mahadev <rahul.mahadev7@gmail.com>
2016-02-04 11:09:31 +0530

    Fixed UUID Isuues

--- app/quiz.c
+++ app/quiz.c
@@ -6,14 +6,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <tee_client_api.h>
-#include <ta_hello_world.h>
+#include <ta_quiz.h>
 #include <err.h>
 int main(void){
  	TEEC_Result res;
 	TEEC_Context ctx;
 	TEEC_Session sess;
+	int inp_val;
 	TEEC_Operation op;
-	TEEC_UUID uuid = TA_HELLO_WORLD_UUID;
+	TEEC_UUID uuid = TA_QUIZ_UUID;
 	uint32_t err_origin;
 	res = TEEC_InitializeContext(NULL, &ctx); // This basically checks if a TEE is present in the system
 	if (res != TEEC_SUCCESS)
@@ -38,22 +39,19 @@ int main(void){
 	
 	printf("Moving to secure world to start quiz \n");
 	//
-	op.params[0].value.a = 4;
-	res = TEEC_InvokeCommand(&sess, TA_HELLO_WORLD_CMD_START_QUIZ, &op,
+
+	printf("Enter the input value:\n");
+	scanf("%d",&inp_val);
+	printf("Input is %d \n",inp_val );
+	op.params[0].value.a = inp_val;
+	res = TEEC_InvokeCommand(&sess, TA_QUIZ_CMD_START_QUIZ, &op,
 				 &err_origin);
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res, err_origin);
 		
 	printf("TEE returned value  %d\n", op.params[0].value.a);
-	/*
-		if(op.params[0].value.a == 1000)
-		{
-			printf("1000 returned \n");
-			break;
-		}
-	*/
 	
-	printf("Quiz eneded\n");
+	printf("Quiz ended\n");
 	/* 
 	 * Close the session 
 	 *
--- out/ta/ta_quiz/.user_ta_header.o.d
+++ out/ta/ta_quiz/.user_ta_header.o.d
@@ -1,17 +1,16 @@
-/home/rahulmahdev/devel/optee/build/../quiz_test/out/ta/fact/hello_world_ta.o: \
- hello_world_ta.c \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_internal_api.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_api.h \
- /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stddef.h \
+/home/rahulmahdev/devel/optee/build/../quiz_test/out/ta/ta_quiz/user_ta_header.o: \
+ /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/src/user_ta_header.c \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/compiler.h \
+ /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_ta_api.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_api_defines.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_api_types.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/stdint.h \
  /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stdbool.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_ta_api.h \
+ /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stddef.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_internal_api_extensions.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/trace.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/trace_levels.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/stdio.h \
  /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stdarg.h \
- include/ta_hello_world.h
+ /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/user_ta_header.h \
+ user_ta_header_defines.h include/ta_quiz.h
