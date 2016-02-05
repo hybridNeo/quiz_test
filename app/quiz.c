5d2c1bfce475c56173f1b8dfc450db8c40fc1704
rahul mahadev <rahul.mahadev7@gmail.com>
2016-02-05 12:06:33 +0530

    Iteration based on questions

--- app/quiz.c
+++ app/quiz.c
@@ -12,7 +12,6 @@ int main(void){
  	TEEC_Result res;
 	TEEC_Context ctx;
 	TEEC_Session sess;
-	int inp_val;
 	TEEC_Operation op;
 	TEEC_UUID uuid = TA_QUIZ_UUID;
 	uint32_t err_origin;
@@ -33,28 +32,24 @@ int main(void){
 	 */
 
 	memset(&op, 0, sizeof(op));
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_NONE,
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_OUTPUT, TEEC_VALUE_OUTPUT,
 					 TEEC_NONE, TEEC_NONE);
 	
-	
-	printf("Moving to secure world to start quiz \n");
-	//
-
-	printf("Enter the input value:\n");
-	scanf("%d",&inp_val);
-	printf("Input is %d \n",inp_val );
-	op.params[0].value.a = inp_val;
-	res = TEEC_InvokeCommand(&sess, TA_QUIZ_CMD_START_QUIZ, &op,
-				 &err_origin);
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res, err_origin);
+	// op.params[0].value.a = 1;
+	printf("********************** QUIZ **************************\n");
+	while(1){
+		res = TEEC_InvokeCommand(&sess, TA_QUIZ_CMD_START_QUIZ, &op,&err_origin);
+		if (res != TEEC_SUCCESS)
+			errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res, err_origin);	
+		printf("TEE returned value  %d\n", op.params[0].value.a);
+		if(op.params[0].value.a == 0){
+			break;
+		}
 		
-	printf("TEE returned value  %d\n", op.params[0].value.a);
-	
+	}	
 	printf("Quiz ended\n");
 	/* 
 	 * Close the session 
-	 *
 	 */
 	TEEC_CloseSession(&sess);
 
