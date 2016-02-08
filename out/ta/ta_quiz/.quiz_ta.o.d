71b13fe8954347fcf73d9fede555eed341767c07
rahul mahadev <rahul.mahadev7@gmail.com>
2016-02-08 21:44:47 +0530

    added ta

--- out/ta/ta_quiz/.quiz_ta.o.d
+++ out/ta/ta_quiz/.quiz_ta.o.d
@@ -1,31 +1,11 @@
 /home/rahulmahdev/devel/optee/build/../quiz_test/out/ta/ta_quiz/quiz_ta.o: \
  quiz_ta.c \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/utee_defines.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/utee_types.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/assert.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/compiler.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/stdint.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/inttypes.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/mpa.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/mpalib.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/mpalib_config.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/limits.h \
- /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stdbool.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/stdlib.h \
- /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stddef.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/malloc.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/types_ext.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/unistd.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/string.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/sys/cdefs.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_internal_api.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_api.h \
+ /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_ta_api.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_api_defines.h \
  /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_api_types.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_ta_api.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_internal_api_extensions.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/trace.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/trace_levels.h \
- /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/stdio.h \
- /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stdarg.h \
+ /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/stdint.h \
+ /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stdbool.h \
+ /home/rahulmahdev/devel/optee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/4.9.2/include/stddef.h \
+ /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/tee_api.h \
+ /home/rahulmahdev/devel/optee/optee_os/out/arm-plat-vexpress/export-user_ta/include/compiler.h \
  include/ta_quiz.h
